ff69ab86434b3ff3c985d9f2a1ded81cb18c6396
Hà Trang <haachangg1979@gmail.com>
2025-06-23 16:29:47 +0700

    change exam adapter

--- app/src/main/java/vn/tlu/edu/cse/Activity/TakeExamActivity.java
+++ app/src/main/java/vn/tlu/edu/cse/Activity/TakeExamActivity.java
@@ -33,6 +33,7 @@ public class TakeExamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_exam);
+        setContentView(R.layout.activity_take_exam);
 
         // Khởi tạo các thành phần giao diện
         timerTextView = findViewById(R.id.timerTextView);
